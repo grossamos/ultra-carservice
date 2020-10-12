a33bfe39795b32d7f957f2f7d95070c3ecdcb0d9
Amos Groß <amos.gross@nttdata.com>
2020-10-12 16:54:08 +0200

    Added test for Storage class

--- src/test/java/com/nttdata/carservice/AutomobileDataStorageTest.java
+++ src/test/java/com/nttdata/carservice/AutomobileDataStorageTest.java
@@ -5,11 +5,16 @@ import org.junit.jupiter.api.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AutomobileDataStorageTest {
+
+    private static HashMap<Integer, Automobile> storageMap;
+    private static final File storageFile = new File("./src/test/data/automobiles.json");;
+    private static AutomobileDataStorage automobileDataStorage;
 
     @Nested
     class automobileFileHandling{
@@ -43,7 +48,7 @@ class AutomobileDataStorageTest {
 
             //get data from file and check if id is correct
             automobileDataStorage.getAutomobilesFromFile();
-            assertEquals(automobileDataStorage.getM_allAutomobiles().get(666).getM_name(), "ULTRA");
+            assertEquals(automobileDataStorage.getAutomobileFromID(666).getM_name(), "ULTRA");
         }
 
         @Test
@@ -64,26 +69,90 @@ class AutomobileDataStorageTest {
         }
     }
 
+    @BeforeEach
+    void initStorage(){
+        storageMap = new HashMap<>();
+        automobileDataStorage = new AutomobileDataStorage(storageMap, storageFile);
+    }
 
+    @AfterEach
+    void cleanStorage(){
+        if (storageFile.exists())
+            assertTrue(storageFile.delete());
+    }
 
     @Test
     void addAutomobile() {
+        //check if it allways adds an object to the HashMap, then checks, if an ID was added
+        for (int i = 0; i < 200; i++) {
+            int old_size = storageMap.size();
+            Automobile autoToAdd = new Automobile();
+            automobileDataStorage.addAutomobile(autoToAdd);
+            //checks for valid add
+            assertEquals(old_size + 1, storageMap.size());
+            //checks if ID was set of new Object
+            assertNotEquals(0, autoToAdd.getM_id());
+        }
     }
 
     @Test
     void removeAutomobile() {
+
+        ArrayList<Integer> allIDs = new ArrayList<>();
+        for (int i = 0; i < 66; i++) {
+            Automobile carToAdd = new Automobile();
+            carToAdd.setValue("name", String.valueOf(i ^ 767));
+            carToAdd.generateId(automobileDataStorage);
+            storageMap.put(carToAdd.getM_id(), carToAdd);
+            allIDs.add(carToAdd.getM_id());
+        }
+
+        //check if all cars were removed
+        for (int id : allIDs) {
+            automobileDataStorage.removeAutomobile(id);
+        }
+        assertEquals(0, storageMap.size());
     }
 
     @Test
     void changeAutomobile() {
+        //add a car to map (with two values ~ one to change and one not to change)
+        Automobile autoToUpdate = new Automobile();
+        autoToUpdate.setValue("should be unchanged", "No change");
+        autoToUpdate.setValue("should be changed0099999", "wat");
+        automobileDataStorage.addAutomobile(autoToUpdate);
+
+        //updates car
+        Automobile autoWithUpdate = new Automobile();
+        autoWithUpdate.setValue("should be changed0099999", "Some change");
+        automobileDataStorage.changeAutomobile(autoToUpdate.getM_id(), autoWithUpdate);
+
+        //check for correctness
+        assertEquals("No change", storageMap.get(autoToUpdate.getM_id()).getValue("should be unchanged"));
+        assertEquals("Some change", storageMap.get(autoToUpdate.getM_id()).getValue("should be changed0099999"));
     }
 
     @Test
     void checkForInvalidID() {
+        //try to add a shit ton of items, then check if id is taken (which it should be)
+        for (int i = 0; i < 420; i++) {
+            Automobile automobileToAdd = new Automobile();
+            automobileDataStorage.addAutomobile(automobileToAdd);
+            assertFalse(automobileDataStorage.checkForInvalidID(automobileToAdd.getM_id()));
+        }
+
     }
 
     @Test
     void clearM_allAutomobiles() {
+        //add dummy data
+        for (int i = 0; i < 69; i++) {
+            automobileDataStorage.addAutomobile(new Automobile());
+        }
+
+        //remove dummy data and check if it's still there
+        automobileDataStorage.clearM_allAutomobiles();
+        assertEquals(0, storageMap.size());
     }
 
 }
