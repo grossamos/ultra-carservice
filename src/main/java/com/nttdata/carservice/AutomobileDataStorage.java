39453b7a33ef9dbed1762857b2689abae615a4ab
Amos Groß <amos.gross@nttdata.com>
2020-10-14 13:07:03 +0200

    Added Javadocs for AutomobileDataStorage

--- src/main/java/com/nttdata/carservice/AutomobileDataStorage.java
+++ src/main/java/com/nttdata/carservice/AutomobileDataStorage.java
@@ -2,7 +2,6 @@ package com.nttdata.carservice;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -10,17 +9,36 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Scanner;
 
+/**
+ * DataStorage class for Automobile entries.
+ *
+ * Provides an interface to interact with and store Automobile Objects.
+ *
+ * @author "Amos Gross"
+ * @version 0.0.1
+ */
 
 public class AutomobileDataStorage {
     private HashMap<Integer, Automobile> m_allAutomobiles;
     private final File m_automobilesFile;
     static private final Gson m_gson = new Gson();
 
+    /**
+     * Constructor for AutomobileDataStorage Dependency Injection.
+     *
+     * Injects dependency of storage class (hashmap) and of where to save the file later on.
+     * @param allAutomobiles Injected storage
+     * @param fileToSave Injected file save location
+     */
 
     public AutomobileDataStorage(HashMap<Integer, Automobile> allAutomobiles, File fileToSave){
         this.m_allAutomobiles = allAutomobiles;
         this.m_automobilesFile = fileToSave;
     }
+
+    /**
+     * Retrieves saved state from the file.
+     */
 
     public void getAutomobilesFromFile(){
         try {
@@ -35,6 +53,10 @@ public class AutomobileDataStorage {
         }
     }
 
+    /**
+     * Saves the current state to the file.
+     */
+
     public void pushAutomobilesToFile(){
         try {
             //noinspection ResultOfMethodCallIgnored
@@ -47,16 +69,36 @@ public class AutomobileDataStorage {
         }
     }
 
+    /**
+     * Adds an Automobile to storage.
+     *
+     * Adds it to the Hashmap and generates a new unique id. <u>Only to be used for initial adding</u>
+     * @param someAutomobile
+     */
+
     public void addAutomobile(Automobile someAutomobile){
         someAutomobile.generateId(this);
         m_allAutomobiles.put(someAutomobile.getM_id(), someAutomobile);
         pushAutomobilesToFile();
     }
 
+    /**
+     * Removes an Automobile from storage
+     *
+     * @param id Id of Automobile to remove
+     */
+
     public void removeAutomobile(int id){
         m_allAutomobiles.remove(id);
         pushAutomobilesToFile();
     }
+
+    /**
+     * Updater for entries.
+     *
+     * @param id
+     * @param changesAutomobile
+     */
 
     public void changeAutomobile(int id, Automobile changesAutomobile){
         Automobile oldAutomobile = m_allAutomobiles.get(id);
@@ -65,8 +107,25 @@ public class AutomobileDataStorage {
         }
     }
 
+    /**
+     * Checks if you would get a valid Object if requesting ID.
+     *
+     * @param id ID to check
+     */
+
     public boolean checkForInvalidID(int id){
         return m_allAutomobiles.get(id) == null;
+    }
+
+    /**
+     * Empties out the storage.
+     *
+     * Deletes all Entries that are in storage.
+     */
+
+    public void clearM_allAutomobiles(){
+        m_allAutomobiles.clear();
+        pushAutomobilesToFile();
     }
 
     public HashMap<Integer, Automobile> getM_allAutomobiles() {
@@ -75,10 +134,5 @@ public class AutomobileDataStorage {
 
     public Automobile getAutomobileFromID(int id){
         return m_allAutomobiles.get(id);
-    }
-
-    public void clearM_allAutomobiles(){
-        m_allAutomobiles.clear();
-        pushAutomobilesToFile();
     }
 }
