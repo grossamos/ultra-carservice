cd12537432b9e3c29bcc4b5fe394abc36efd7d17
Amos Groß <amos.gross@nttdata.com>
2020-10-23 14:41:20 +0200

    Corrected tests, to work with new database

--- src/test/java/com/nttdata/carservice/AutomobileDataStorageTest.java
+++ src/test/java/com/nttdata/carservice/AutomobileDataStorageTest.java
@@ -2,159 +2,56 @@ package com.nttdata.carservice;
 
 import com.nttdata.carservice.entity.Automobile;
 import com.nttdata.carservice.storage.AutomobileDataStorage;
+import com.nttdata.carservice.storage.AutomobileRepo;
 import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AutomobileDataStorageTest {
 
-    private static HashMap<Integer, Automobile> storageMap;
-    private static final File storageFile = new File("./src/test/data/automobiles.json");;
-    private static AutomobileDataStorage automobileDataStorage;
+    private static final AutomobileRepo automobileRepo = Mockito.mock(AutomobileRepo.class);
+    private static final Automobile emptyAutomobile = Mockito.mock(Automobile.class);
+    private static AutomobileDataStorage automobileDataStorage = new AutomobileDataStorage(automobileRepo);
 
-    @Nested
-    class automobileFileHandling{
-
-        private final File file = new File("./src/test/data/invalidFilePath.json");
-        private FileWriter fileWriter;
-
-        @BeforeEach
-        void setFileWriter(){
-            try {
-                fileWriter = new FileWriter(file);
-            } catch (IOException e) {
-                e.printStackTrace();
-                fail();
-            }
-        }
-
-        @AfterEach
-        void cleanUpFile() throws IOException {
-            fileWriter.close();
-            assertTrue(file.delete());
-        }
-
-        @Test
-        void getAutomobilesFromFile() throws IOException {
-            //create file with test contents
-            assertNotNull(fileWriter);
-            fileWriter.write("{\"666\":{\"m_id\":666,\"m_automobileAttributes\":{\"name\":\"ULTRA\"}}}");
-            AutomobileDataStorage automobileDataStorage = new AutomobileDataStorage(null);
-            fileWriter.close();
-
-            //get data from file and check if id is correct
-//            automobileDataStorage.getAutomobilesFromFile();
-//            assertEquals(automobileDataStorage.getAutomobileFromID(666).getM_name(), "ULTRA");
-        }
-
-        @Test
-        void pushAutomobilesToFile() throws IOException {
-            //create testClass
-            AutomobileDataStorage emptyAutomobileDataStorage = new AutomobileDataStorage(null);
-
-            //check if new file is created
-            fileWriter.close();
-            assertTrue(file.delete());
-            assertFalse(file.exists());
-//            emptyAutomobileDataStorage.pushAutomobilesToFile();
-            assertTrue(file.exists());
-
-            //add data to hashmap and check if file contains something
-            emptyAutomobileDataStorage.addAutomobile(new Automobile());
-            assertNotEquals(0, file.length());
-        }
-    }
 
     @BeforeEach
     void initStorage(){
-        storageMap = new HashMap<>();
-        automobileDataStorage = new AutomobileDataStorage(null);
-    }
-
-    @AfterEach
-    void cleanStorage(){
-        if (storageFile.exists())
-            assertTrue(storageFile.delete());
     }
 
     @Test
-    void addAutomobile() {
-        //check if it allways adds an object to the HashMap, then checks, if an ID was added
-        for (int i = 0; i < 200; i++) {
-            int old_size = storageMap.size();
-            Automobile autoToAdd = new Automobile();
-            automobileDataStorage.addAutomobile(autoToAdd);
-            //checks for valid add
-            assertEquals(old_size + 1, storageMap.size());
-            //checks if ID was set of new Object
-            assertNotEquals(0, autoToAdd.getM_id());
-        }
+    void check_if_addAutomobile_added_car_to_repo(){
+
+        automobileDataStorage.addAutomobile(emptyAutomobile);
+        Mockito.verify(automobileRepo, Mockito.times(1)).save(Mockito.any(Automobile.class));
     }
 
     @Test
-    void removeAutomobile() {
-
-        ArrayList<Integer> allIDs = new ArrayList<>();
-        for (int i = 0; i < 66; i++) {
-            Automobile carToAdd = new Automobile();
-            carToAdd.setValue("name", String.valueOf(i ^ 767));
-            carToAdd.generateId(automobileDataStorage);
-            storageMap.put(carToAdd.getM_id(), carToAdd);
-            allIDs.add(carToAdd.getM_id());
-        }
-
-        //check if all cars were removed
-        for (int id : allIDs) {
-            automobileDataStorage.removeAutomobile(id);
-        }
-        assertEquals(0, storageMap.size());
+    void check_if_removeAutomobile_removed_car_from_repo(){
+        Mockito.when(automobileRepo.findById(Mockito.anyInt())).thenReturn(Optional.of(emptyAutomobile));
+        automobileDataStorage.removeAutomobile(420);
+        Mockito.verify(automobileRepo, Mockito.times(1)).findById(Mockito.anyInt());
     }
 
     @Test
-    void changeAutomobile() {
+    void check_if_changeAutomobile_() {
         //add a car to map (with two values ~ one to change and one not to change)
         Automobile autoToUpdate = new Automobile();
         autoToUpdate.setValue("should be unchanged", "No change");
-        autoToUpdate.setValue("should be changed0099999", "wat");
-        automobileDataStorage.addAutomobile(autoToUpdate);
+        autoToUpdate.setValue("should be changed", "No change");
+        Mockito.when(automobileRepo.findById(Mockito.anyInt())).thenReturn(Optional.of(autoToUpdate));
 
         //updates car
         Automobile autoWithUpdate = new Automobile();
-        autoWithUpdate.setValue("should be changed0099999", "Some change");
+        autoWithUpdate.setValue("should be changed", "Some change");
         automobileDataStorage.changeAutomobile(autoToUpdate.getM_id(), autoWithUpdate);
 
         //check for correctness
-        assertEquals("No change", storageMap.get(autoToUpdate.getM_id()).getValue("should be unchanged"));
-        assertEquals("Some change", storageMap.get(autoToUpdate.getM_id()).getValue("should be changed0099999"));
-    }
-
-    @Test
-    void checkForInvalidID() {
-        //try to add a shit ton of items, then check if id is taken (which it should be)
-        for (int i = 0; i < 420; i++) {
-            Automobile automobileToAdd = new Automobile();
-            automobileDataStorage.addAutomobile(automobileToAdd);
-            assertFalse(automobileDataStorage.checkForInvalidID(automobileToAdd.getM_id()));
-        }
-
-    }
-
-    @Test
-    void clearM_allAutomobiles() {
-        //add dummy data
-        for (int i = 0; i < 69; i++) {
-            automobileDataStorage.addAutomobile(new Automobile());
-        }
-
-        //remove dummy data and check if it's still there
-        automobileDataStorage.clearM_allAutomobiles();
-        assertEquals(0, storageMap.size());
+        assertEquals("No change", autoToUpdate.getValue("should be unchanged"));
+        assertEquals("Some change", autoWithUpdate.getValue("should be changed"));
     }
 
 }
--- src/test/java/com/nttdata/carservice/controller/AutomobileControllerTest.java
+++ src/test/java/com/nttdata/carservice/controller/AutomobileControllerTest.java
@@ -2,50 +2,66 @@ package com.nttdata.carservice.controller;
 
 import com.nttdata.carservice.entity.Automobile;
 import com.nttdata.carservice.storage.AutomobileDataStorage;
+import com.nttdata.carservice.storage.AutomobileRepo;
 import org.apache.log4j.Logger;
 import org.junit.jupiter.api.*;
 import org.mockito.Mockito;
 import org.springframework.http.HttpStatus;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class AutomobileControllerTest {
 
-    static AutomobileController automobileController;
-    static AutomobileDataStorage automobileDataStorage = Mockito.mock(AutomobileDataStorage.class);
     static Automobile emptyAutomobile = Mockito.mock(Automobile.class);
+    static Map<String, String> emptyHashmap = Mockito.mock(HashMap.class);
+    static AutomobileRepo automobileRepo = Mockito.mock(AutomobileRepo.class);
+    static ArrayList<Automobile> mockArrayList = Mockito.mock(ArrayList.class);
+    static AutomobileDataStorage automobileDataStorage = Mockito.mock(AutomobileDataStorage.class);
+    static AutomobileController automobileController = new AutomobileController(automobileRepo, automobileDataStorage);
 
     @BeforeAll
     static void setAutomobileController(){
-        //initialize Controller with Mock Database
-        automobileController = new AutomobileController(null);
-
-        //disable Logger
+        //disable logger
         Logger.getRootLogger().removeAllAppenders();
+
+        //setup mockito objects
+        Mockito.when(automobileDataStorage.getM_allAutomobiles()).thenReturn(mockArrayList);
+        Mockito.when(emptyAutomobile.getM_automobileAttributes()).thenReturn(emptyHashmap);
+        Mockito.when(emptyHashmap.isEmpty()).thenReturn(false);
     }
 
     @Test
     void readSingleAutomobile() {
         //check ID validity checker
         setMockIdCheckToTrue();
-        assertEquals(HttpStatus.BAD_REQUEST, automobileController.readSingleAutomobile(0).getStatusCode());
+        assertEquals(HttpStatus.NOT_FOUND, automobileController.readSingleAutomobile(0).getStatusCode());
 
         //setup Mockito checkForInvalidID to say every ID is valid, and return empty Automobile at getAutomobileFromID
         setMockIdCheckToFalse();
-//        Mockito.when(automobileDataStorage.getAutomobileFromID(Mockito.anyInt())).thenReturn(emptyAutomobile);
+        Mockito.when(automobileDataStorage.getAutomobileFromID(Mockito.anyInt())).thenReturn(emptyAutomobile);
 
         //test if Controller returns empty Automobile as Response code, with correct code
         assertEquals(HttpStatus.OK, automobileController.readSingleAutomobile(0).getStatusCode());
     }
 
     @Test
-    void readAllAutomobile() {
+    void readAllAutomobile_with_empty_map() {
         //setup mockito to return mock Hashmap
-        HashMap<Integer, Automobile> emptyMap = Mockito.mock(HashMap.class);
-//        Mockito.when(automobileDataStorage.getM_allAutomobiles()).thenReturn(emptyMap);
+        setIsEmptyCheckTo(true);
+
+        //check if Map is returned with correct code
+        assertEquals(HttpStatus.NO_CONTENT, automobileController.readAllAutomobile().getStatusCode());
+    }
+
+    @Test
+    void readAllAutomobile_with_non_empty_map() {
+        //setup mockito to return mock Hashmap
+        setIsEmptyCheckTo(false);
 
         //check if Map is returned with correct code
         assertEquals(HttpStatus.OK, automobileController.readAllAutomobile().getStatusCode());
@@ -55,23 +71,24 @@ public class AutomobileControllerTest {
     void updateAutomobile() {
         //check ID validity checker
         setMockIdCheckToTrue();
-        assertEquals(HttpStatus.BAD_REQUEST, automobileController.updateAutomobile(emptyAutomobile, 99).getStatusCode());
+        assertEquals(HttpStatus.NOT_FOUND, automobileController.updateAutomobile(emptyAutomobile, 99).getStatusCode());
 
         //check Response code
         setMockIdCheckToFalse();
+        setIsEmptyCheckTo(false);
         assertEquals(HttpStatus.OK, automobileController.updateAutomobile(emptyAutomobile, 99).getStatusCode());
     }
 
     @Test
-    void createAutomobile() {
-        assertEquals(HttpStatus.OK, automobileController.createAutomobile(emptyAutomobile).getStatusCode());
+    void check_response_code_of_createAutomobile() {
+        assertEquals(HttpStatus.CREATED, automobileController.createAutomobile(emptyAutomobile).getStatusCode());
     }
 
     @Test
     void deleteAutomobile() {
         //check ID validity checker
         setMockIdCheckToTrue();
-        assertEquals(HttpStatus.BAD_REQUEST, automobileController.deleteAutomobile(99).getStatusCode());
+        assertEquals(HttpStatus.NOT_FOUND, automobileController.deleteAutomobile(99).getStatusCode());
 
         //check Response code
         setMockIdCheckToFalse();
@@ -81,6 +98,10 @@ public class AutomobileControllerTest {
     @Test
     void resetAllAutomobiles() {
         assertEquals(HttpStatus.NO_CONTENT, automobileController.resetAllAutomobiles().getStatusCode());
+    }
+
+    void setIsEmptyCheckTo(boolean returnBool){
+        Mockito.when(mockArrayList.isEmpty()).thenReturn(returnBool);
     }
 
     void setMockIdCheckToFalse(){
